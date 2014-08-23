54e6de0e9446b0dd38699a2108e601e5fc70f34a
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-22 21:16:19 -0500

    email generate script

--- email.py
+++ email.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__':
 	f = open("ec-committees.txt")
+	o = open("ec-email.txt", "w")
 	for i in f:
 		email = i.strip('\n')
+		if email:
+			o.write("%s@illinois.edu\n" %email)
