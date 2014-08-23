6f43cfd6a4e6b8abd7c7250918ac6dd743fbb468
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-22 21:10:32 -0500

    table temple

---
+++ email.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+	f = open("ec-committees.txt")
+	for i in f:
+		email = i.strip('\n')
---
+++ table.py
@@ -0,0 +1,21 @@
+from xlsx import Workbook
+from jinja2 import Environment, FileSystemLoader
+
+if __name__ == '__main__':
+	book = Workbook("E-Night List.xlsx")
+	for sheet in book:
+		table = []
+		for row, cells in sheet.rows().iteritems():
+			if row != 1:
+				temp = []
+				for cell in cells:
+					temp.append(cell.value)
+				table.append(temp)
+
+		env = Environment(loader=FileSystemLoader('templates'))
+		template = env.get_template('table-template.html')
+		output_from_parsed_template = template.render(table=table)
+
+		with open("table.html", "wb") as fh:
+			fh.write(output_from_parsed_template.encode('ascii', 'xmlcharrefreplace'))
+	
