1ceace35a709ecef8d9cd50338f2fbea89075b03
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-08-26 08:52:10 -0500

    update massmail.py

--- massmail.py
+++ massmail.py
@@ -1,6 +1,17 @@
+# generate lists of email for massmail 
+
 if __name__ == '__main__':
 	f = open("massmail.txt")
+	count = 1
+	list_email = []
 	for i in f:
 		email = i.strip('\n')
 		if email:
-			print "%s \\" %email
+			list_email.append("%s \\\n" %email)
+			if (len(list_email) == 3000):
+				f = open("list%d.txt" %count, "w")
+				f.writelines(list_email)
+				list_email = []
+				count += 1
+	f = open("list%d.txt" %count, "w")
+	f.writelines(list_email)
